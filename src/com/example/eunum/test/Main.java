aa122e8b7033c44cf27b73bf40baba7eb23ada46
tomek <tomeksochacki@wp.pl>
2020-04-09 23:37:21 +0200

    IInitialize repository

---
+++ src/com/example/eunum/test/Main.java
@@ -0,0 +1,16 @@
+package com.example.eunum.test;
+
+public class Main {
+    public static void main(String[] args) {
+
+        WeekDays weekDays1 = WeekDays.MONDAY;
+        WeekDays weekDays2 = WeekDays.TUESDAY;
+        WeekDays weekDays3 = WeekDays.WEDNESDAY;
+        WeekDays weekDays4 = WeekDays.THURSDAY;
+        WeekDays weekDays5 = WeekDays.FRIDAY;
+        WeekDays weekDays6 = WeekDays.SATURDAY;
+        WeekDays weekDays7 = WeekDays.SUNDAY;
+
+        System.out.println("1: " + weekDays1 + ", 2: " + weekDays2 + ", 3: " + weekDays3 + ", 4: " + weekDays4 + ", 5: " + weekDays5 + ", 6: " + weekDays6 + ", 7: " + weekDays7 );
+    }
+}
---
+++ src/com/example/eunum/test/WeekDays.java
@@ -0,0 +1,12 @@
+package com.example.eunum.test;
+
+public enum WeekDays {
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
